f7df343aca061bde6666dea5ba80fc442aaa9539
akdbsdud6 <95210870+akdbsdud6@users.noreply.github.com>
2025-03-03 02:17:42 -0800

    Add files via upload

---
+++ asgn4_final/src/Camera.js
@@ -0,0 +1,74 @@
+class Camera {
+  constructor() {
+    this.fov = 60;
+    this.eye = new Vector3([0.0, 0.0, -5.0]);
+    this.up = new Vector3([0.0, 1.0, 0.0]);
+    this.at = new Vector3([0.0, 0.0, -1.0]);
+    this.alpha = 5;
+
+    this.viewMat = new Matrix4();
+    this.updateViewMatrix();
+    gl.uniformMatrix4fv(u_ViewMatrix, false, this.viewMat.elements);
+
+    this.projMat = new Matrix4();
+    this.projMat.setPerspective(50, canvas.width / canvas.height, 1, 100);
+    gl.uniformMatrix4fv(u_ProjectionMatrix, false, this.projMat.elements);
+  }
+
+  updateViewMatrix() {
+    this.viewMat.setLookAt(
+      this.eye.elements[0], this.eye.elements[1], this.eye.elements[2],
+      this.at.elements[0], this.at.elements[1], this.at.elements[2],
+      this.up.elements[0], this.up.elements[1], this.up.elements[2]
+    );
+  }
+
+  moveForward(speed) {
+    let direction = new Vector3([0, 0, 0]);
+    direction.set(this.at);
+    direction.sub(this.eye);
+    if (direction.elements[0] === 0 && direction.elements[1] === 0 && direction.elements[2] === 0) {
+      direction.set([0, 0, 1]);
+    }
+    direction.normalize();
+    direction.mul(speed);
+    this.eye.add(direction);
+    this.at.add(direction);
+  }
+
+  moveBackwards(speed) {
+    let direction = new Vector3([0, 0, 0]);
+    direction.set(this.eye);
+    direction.sub(this.at);
+    if (direction.elements[0] === 0 && direction.elements[1] === 0 && direction.elements[2] === 0) {
+      direction.set([0, 0, 1]);
+    }
+    direction.normalize();
+    direction.mul(speed);
+    this.eye.add(direction);
+    this.at.add(direction);
+  }
+
+  moveLeft(speed) {
+    let forward = new Vector3([0, 0, 0]);
+    forward.set(this.at);
+    forward.sub(this.eye);
+    let leftDir = Vector3.cross(this.up, forward);
+    leftDir.normalize();
+    leftDir.mul(speed);
+    this.eye.add(leftDir);
+    this.at.add(leftDir);
+  }
+
+  moveRight(speed) {
+    let forward = new Vector3([0, 0, 0]);
+    forward.set(this.at);
+    forward.sub(this.eye);
+    let rightDir = Vector3.cross(forward, this.up);
+    rightDir.normalize();
+    rightDir.mul(speed);
+    this.eye.add(rightDir);
+    this.at.add(rightDir);
+  }
+
+}
---
+++ asgn4_final/src/Cube.js
@@ -0,0 +1,161 @@
+class Cube {
+  constructor() {
+    this.type = 'cube';
+    this.color = [1.0, 1.0, 1.0, 1.0];
+    this.matrix = new Matrix4();
+    this.textureNum = -2;
+  }
+
+  drawTriangle3DNormalUV(vertices, uv, normals) {
+    let vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_Position);
+
+    let uvBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, uvBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uv), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(a_UV, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_UV);
+
+    let normalBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(a_Normal, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_Normal);
+
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+  }
+
+  drawCube(M) {
+    gl.uniformMatrix4fv(u_ModelMatrix, false, M.elements);
+    gl.uniform1i(u_whichTexture, this.textureNum);
+
+    // ----- FRONT FACE -----
+    // Normal: (0,0,-1)
+    gl.uniform4f(
+      u_FragColor,
+      this.color[0],
+      this.color[1],
+      this.color[2],
+      this.color[3]
+    );
+    let frontNormal = [0.0, 0.0, -1.0];
+    this.drawTriangle3DNormalUV(
+      [0,0,0, 1,1,0, 1,0,0],
+      [0,0, 1,1, 1,0],
+      [...frontNormal, ...frontNormal, ...frontNormal]
+    );
+    this.drawTriangle3DNormalUV(
+      [0,0,0, 0,1,0, 1,1,0],
+      [0,0, 0,1, 1,1],
+      [...frontNormal, ...frontNormal, ...frontNormal]
+    );
+
+    // ----- TOP FACE -----
+    // Normal: (0,1,0)
+    //gl.uniform4f(
+      u_FragColor,
+      this.color[0],
+      this.color[1],
+      this.color[2],
+      this.color[3]
+    //);
+    let topNormal = [0.0, 1.0, 0.0];
+    this.drawTriangle3DNormalUV(
+      [0,1,0, 0,1,1, 1,1,1],
+      [0,0, 0,1, 1,1],
+      [...topNormal, ...topNormal, ...topNormal]
+    );
+    this.drawTriangle3DNormalUV(
+      [0,1,0, 1,1,1, 1,1,0],
+      [0,0, 1,1, 1,0],
+      [...topNormal, ...topNormal, ...topNormal]
+    );
+
+    // ----- BACK FACE -----
+    // Normal: (0,0,1)
+    //gl.uniform4f(
+      u_FragColor,
+      this.color[0],
+      this.color[1],
+      this.color[2],
+      this.color[3]
+    //);
+    let backNormal = [0.0, 0.0, 1.0];
+    this.drawTriangle3DNormalUV(
+      [0,0,1, 1,0,1, 1,1,1],
+      [0,0, 1,0, 1,1],
+      [...backNormal, ...backNormal, ...backNormal]
+    );
+    this.drawTriangle3DNormalUV(
+      [0,0,1, 1,1,1, 0,1,1],
+      [0,0, 1,1, 0,1],
+      [...backNormal, ...backNormal, ...backNormal]
+    );
+
+    // ----- LEFT FACE -----
+    // Normal: (-1,0,0)
+    //gl.uniform4f(
+      u_FragColor,
+      this.color[0],
+      this.color[1],
+      this.color[2],
+      this.color[3]
+    //);
+    let leftNormal = [-1.0, 0.0, 0.0];
+    this.drawTriangle3DNormalUV(
+      [0,0,0, 0,0,1, 0,1,1],
+      [0,0, 1,0, 1,1],
+      [...leftNormal, ...leftNormal, ...leftNormal]
+    );
+    this.drawTriangle3DNormalUV(
+      [0,0,0, 0,1,1, 0,1,0],
+      [0,0, 1,1, 0,1],
+      [...leftNormal, ...leftNormal, ...leftNormal]
+    );
+
+    // ----- RIGHT FACE -----
+    // Normal: (1,0,0)
+    //gl.uniform4f(
+      u_FragColor,
+      this.color[0],
+      this.color[1],
+      this.color[2],
+      this.color[3]
+    //);
+    let rightNormal = [1.0, 0.0, 0.0];
+    this.drawTriangle3DNormalUV(
+      [1,0,0, 1,1,1, 1,0,1],
+      [0,0, 1,1, 1,0],
+      [...rightNormal, ...rightNormal, ...rightNormal]
+    );
+    this.drawTriangle3DNormalUV(
+      [1,0,0, 1,1,0, 1,1,1],
+      [0,0, 0,1, 1,1],
+      [...rightNormal, ...rightNormal, ...rightNormal]
+    );
+
+    // ----- BOTTOM FACE -----
+    // Normal: (0,-1,0)
+    //gl.uniform4f(
+      u_FragColor,
+      this.color[0],
+      this.color[1],
+      this.color[2],
+      this.color[3]
+    //);
+    let bottomNormal = [0.0, -1.0, 0.0];
+    this.drawTriangle3DNormalUV(
+      [0,0,0, 1,0,1, 0,0,1],
+      [0,0, 1,1, 0,1],
+      [...bottomNormal, ...bottomNormal, ...bottomNormal]
+    );
+    this.drawTriangle3DNormalUV(
+      [0,0,0, 1,0,0, 1,0,1],
+      [0,0, 1,0, 1,1],
+      [...bottomNormal, ...bottomNormal, ...bottomNormal]
+    );
+  }
+}
